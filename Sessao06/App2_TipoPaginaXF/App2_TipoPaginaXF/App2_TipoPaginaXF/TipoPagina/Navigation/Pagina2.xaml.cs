5d045de8c358bac8ac93b89d069b86e7e38d274e
Jonas Carvalho de Araujo <jonasca231094@gmail.com>
2018-11-20 23:53:57 -0200

    Criado

---
+++ Sessao06/App2_TipoPaginaXF/App2_TipoPaginaXF/App2_TipoPaginaXF/TipoPagina/Navigation/Pagina2.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2_TipoPaginaXF.TipoPagina.Navigation
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Pagina2 : ContentPage
+	{
+		public Pagina2 ()
+		{
+			InitializeComponent ();
+		}
+
+        private void MudarParaPaginaModal(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Pagina1());
+        }
+
+    }
+}
